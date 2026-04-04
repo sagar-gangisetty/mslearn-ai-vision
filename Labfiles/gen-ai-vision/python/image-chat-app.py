a30b1ef4dcdd6aab8ec57b1204a82cac248655ab
sagar-gangisetty <sagar.gangisetty@outlook.com>
2026-04-04 14:21:48 +0000

    Update environment variables and enhance image chat application with Azure OpenAI integration

--- Labfiles/gen-ai-vision/python/image-chat-app.py
+++ Labfiles/gen-ai-vision/python/image-chat-app.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Add references
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from openai import AzureOpenAI
 
 
 def main(): 
@@ -21,8 +24,17 @@ def main():
 
 
         # Create an OpenAI client
-        
+        # Initialize the project client
+        project_client = AIProjectClient(            
+                    credential=DefaultAzureCredential(
+                                    exclude_environment_credential=True,
+                                                exclude_managed_identity_credential=True
+                    ),
+                            endpoint=openai_endpoint,
+        )
 
+        # Get a chat client
+        openai_client = project_client.get_openai_client()
 
         # Initialize prompts
         system_message = "You are an AI assistant in a grocery store that sells fruit. You provide detailed answers to questions about produce."
@@ -34,13 +46,31 @@ def main():
             if prompt.lower() == "quit":
                 break
             elif len(prompt) == 0:
-                    print("Please enter a question.\n")
+                print("Please enter a question.\n")
             else:
                 print("Getting a response ...\n")
 
-
                 # Get a response to image input
-                    
+                image_url = "https://github.com/MicrosoftLearning/mslearn-ai-vision/raw/refs/heads/main/Labfiles/gen-ai-vision/mango.jpeg"
+                image_format = "jpeg"
+                request = Request(image_url, headers={"User-Agent": "Mozilla/5.0"})
+                image_data = base64.b64encode(urlopen(request).read()).decode("utf-8")
+                data_url = f"data:image/{image_format};base64,{image_data}"
+
+                response = openai_client.chat.completions.create(
+                    model=model_deployment,
+                    messages=[
+                        {"role": "system", "content": system_message},
+                        { "role": "user", "content": [  
+                            { "type": "text", "text": prompt},
+                            { "type": "image_url", "image_url": {"url": data_url}}
+                        ] } 
+                    ]
+                )
+                print(response.choices[0].message.content)
+
+                
+
 
 
     except Exception as ex:
